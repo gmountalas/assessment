18536b43093fca2e32415e77a6b411d395cdb631
gmountalas <mountalasgeorge@gmail.com>
2021-02-11 17:47:36 +0200

    api fetch

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('https://api.satisupp.com/pokedex').
+then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      return Promise.reject('something went wrong!')
+    }
+  }).
+then(data => console.log(data));</script>
